872c5b5712752abd38529d2150fbb3d3c920c7ac
David Gros <dgros@openstudio.fr>
2015-10-26 10:08:30 +0100

    script to update '0xxxx' id and zip code

---
+++ Setup/update_insee_geo_id.sql
@@ -0,0 +1,40 @@
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- Update de le table department
+UPDATE `insee_geo_department`
+SET `region_id` = concat('0',`region_id`)
+WHERE LENGTH(`region_id`) = 1;
+
+-- Update de le table region
+UPDATE `insee_geo_region`
+SET `id` = concat('0',`id`)
+WHERE LENGTH(`id`) = 1;
+UPDATE `insee_geo_region`
+SET `prefecture_id` = concat('0',`prefecture_id`)
+WHERE LENGTH(`prefecture_id`) = 4;
+
+-- Update de la table region i18n
+UPDATE `insee_geo_region_i18n`
+SET `id` = concat('0',`id`)
+WHERE LENGTH(`id`) = 1;
+
+-- Update de le table municipality
+UPDATE `insee_geo_municipality`
+SET `id` = concat('0',`id`)
+WHERE LENGTH(`id`) = 4;
+UPDATE `insee_geo_municipality`
+SET `zip_code` = concat('0',`zip_code`)
+WHERE LENGTH(`zip_code`) = 4;
+UPDATE `insee_geo_municipality`
+SET `department_id` = concat('0',`department_id`)
+WHERE LENGTH(`department_id`) = 1;
+UPDATE `insee_geo_municipality`
+SET `region_id` = concat('0',`region_id`)
+WHERE LENGTH(`region_id`) = 1;
+
+-- Update de la table region i18n
+UPDATE `insee_geo_municipality_i18n`
+SET `id` = concat('0',`id`)
+WHERE LENGTH(`id`) = 4;
+
+SET FOREIGN_KEY_CHECKS = 1;
